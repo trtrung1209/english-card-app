4b475b2936c7ba4a6e4fcf4e8d2b0570354be24b
trtrung1209 <trtrung1209@gmail.com>
2025-10-02 11:09:02 +0700

    Add assets and basic configuration

---
+++ lib/values/app_assets.dart
@@ -0,0 +1,12 @@
+/*
+  Define path of assets (image)
+*/
+
+class AppAssets {
+  static final String imagePath = 'assets/images';
+  static final String exchange = '$imagePath/exchange.png';
+  static final String heart = '$imagePath/heart.png';
+  static final String leftChevron = '$imagePath/left-chevron.png';
+  static final String rightChevron = '$imagePath/right-chevron.png';
+  static final String menu = '$imagePath/menu.png';
+}
